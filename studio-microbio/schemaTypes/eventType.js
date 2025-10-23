0ecdb17b6bda3c7f1f2f23036134c08ad27e3871
edumu <eduardoaum2007@gmail.com>
2025-10-22 22:53:38 -0300

    Sanity completamente linkada

--- studio-microbio/schemaTypes/eventType.js
+++ studio-microbio/schemaTypes/eventType.js
@@ -1,6 +1,6 @@
 import {defineField, defineType} from 'sanity'
 
-export const artistType = defineType({
+export const noticiasType = defineType({
   name: 'noticias',
   title: 'Noticias',
   type: 'document',
@@ -21,4 +21,74 @@ export const artistType = defineType({
       type: 'image',
     }),
   ],
+})
+
+export const imagensType = defineType({
+  name: 'imagem',
+  title: 'Imagens',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'imagem',
+      title: 'Imagem',
+      type: 'image',
+    }),
+  ],
+})
+
+export const sociaisType = defineType({
+  name: 'sociais',
+  title: 'Redes sociais',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'telefone',
+      title: 'Telefone',
+      type: 'string',
+      validation: (Rule) =>
+        Rule.regex(/^(\+55\s?)?(\(?\d{2}\)?\s?)(\d{4,5}[-\s]?\d{4})$/, {
+          name: 'telefone',
+          invert: false,
+        }).error('Formato de telefone inválido. Tente o formato (XX) XXXXX-XXXX'),
+    }),
+    defineField({
+      name: 'instagram',
+      title: 'instagram',
+      type: 'url',
+    }),
+    defineField({
+      name: 'facebook',
+      title: 'Facebook',
+      type: 'url',
+    }),
+  ],
+
+  preview: {
+    prepare() {
+      return {
+        title: 'Redes sociais',
+        subtitle: 'Contato e links',
+      }
+    },
+  },
+})
+
+export const unilabType = defineType({
+  name: 'unilab',
+  title: 'Link unilab',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'unilab',
+      title: 'Unilab',
+      type: 'url',
+    }),
+  ],
+  preview: {
+    prepare() {
+      return {
+        name: 'Link Unilab',
+      }
+    },
+  },
 })
