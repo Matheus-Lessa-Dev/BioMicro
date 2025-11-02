fac8dc01d44dfaff3ee2bd21384808d3edfc7dd5
edumu <eduardoaum2007@gmail.com>
2025-11-02 05:47:09 -0300

    maps dinamico

--- sites/JS/contatosSanity.js
+++ sites/JS/contatosSanity.js
@@ -1,9 +1,7 @@
 const URL =
   "https://zjpvcus5.api.sanity.io/v2025-11-02/data/query/production?query=*%5B_type+%3D%3D+%22contatos%22%5D%0A%7B+rua%2C%0A++cidade%2C%0A++cep%0A%7D&perspective=drafts";
 (async () => {
-  const response = await fetch(URL, {
-    method: "GET",
-  });
+  const response = await fetch(URL);
 
   const json = await response.json();
   const result = json.result;
